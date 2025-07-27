ab9b6ec1a1aad3844e11685f3c439fab805fc22d
Cristóbal Andrés Cortés Cofré <cristobal.cortes@alu.ucm.cl>
2025-07-27 10:18:17 -0400

    "Formato de base de datos, migraciones"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import PerfilUsuario
 
-# Register your models here.
+class PerfilUsuarioInline(admin.StackedInline):
+    model = PerfilUsuario
+    can_delete = False
+    verbose_name_plural = 'Perfil'
+
+class UserAdminCustom(UserAdmin):
+    inlines = (PerfilUsuarioInline,)
+    
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdminCustom, self).get_inline_instances(request, obj)
+
+# Desregistrar el User admin original y registrar el customizado
+admin.site.unregister(User)
+admin.site.register(User, UserAdminCustom)
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ['user', 'rol', 'telefono', 'activo', 'fecha_ingreso']
+    list_filter = ['rol', 'activo', 'fecha_ingreso']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name']
